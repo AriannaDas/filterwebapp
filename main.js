963a55f705bf3f4ad42e61210079049507e103b4
AriannaDas <83515033+AriannaDas@users.noreply.github.com>
2022-06-30 22:19:53 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.position(650, 140);
+}
+function draw() {
+
+}
+
+function take_snapshot() {
+    save("myFilterImage.png");
+}
