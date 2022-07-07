663e5fc76d3b8324285a319db259eb75b092f804
AriannaDas <83515033+AriannaDas@users.noreply.github.com>
2022-07-07 14:51:11 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function preload() {
+noseX = 0;
+noseY = 0;
 
+function preload() {
+    mustache_image = loadImage("https://i.postimg.cc/Jn1rL86N/moustache-PNG.png");
 }
 
 function setup() {
@@ -15,6 +18,7 @@ function setup() {
 
 function draw() {
     image(video, 0, 0, 300, 300);
+    image(mustache_image, noseX, noseY, 30, 30);
 }
 
 function take_snapshot() {
@@ -28,7 +32,12 @@ function modelLoaded() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x - 15;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
     }
 }
+
+
+
